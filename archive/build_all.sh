b3cf5892c22fb9248a7508c2ae621c0a140e98d7
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2015-12-11 11:55:36 +0000

    resurrect previous build scripts for reference

---
+++ archive/build_all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+REBUILD=''
+NVIDIA=''
+NATIVE=''
+CONCURRENCY='-j4'
+QMAKE=${QMAKE:-/usr/bin/qmake}
+RELEASE='CONFIG+=release CONFIG-=debug'
+
+while test $# -gt 0
+do
+    case "$1" in
+        --rebuild) REBUILD='1'
+            ;;
+        --nvidia) NVIDIA="CONFIG += nvidia"
+            ;;
+        --native) NATIVE="CONFIG += native"
+            ;;
+        --travis) CONCURRENCY="-j1"
+            ;;
+        --debug) RELEASE="CONFIG+=debug CONFIG-=release"
+            ;;
+        --*) echo "bad option $1"; exit 1
+            ;;
+        *) echo "unrecognised argument $1"; exit 1
+            ;;
+    esac
+    shift
+done
+
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+for PROJ in ${DIR}/{Ember,EmberCL,EmberGenome,EmberRender,EmberAnimate,Fractorium}
+do
+  pushd $PROJ
+  if [ "x1" = "x$REBUILD" ]; then
+    make clean
+  fi
+  $QMAKE "$NVIDIA" "$NATIVE" $RELEASE
+  make $CONCURRENCY
+  if [ "x$?" != "x0" ]; then
+    echo "Build failed! Check output for errors."
+    exit 1
+  fi
+  popd
+done
+
